9d8759ce13889c8e1d3fa504724f7171ababd744
Jose <jg764609@southeast.edu>
2022-06-29 21:32:39 -0500

    Extenal Excercises

---
+++ src/Operations.java
@@ -0,0 +1,28 @@
+
+public class Operations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 25; 
+		int y = 2;
+		int z = -10;
+		int answer = x + y * z;
+		System.out.println(answer);
+		
+		String word1 = " Hello "; 
+		String word2 = " World ";
+		String word3 = " Crazy "; 
+		
+		System.out.println(word1 + word3 + word2);
+		
+		//What number will print to console? 
+		
+		int number1 = 6; 
+		int number2 = 3; 
+		
+		System.out.println(number1 % number2 );
+		
+	}
+
+}
---
+++ src/Variables.java
@@ -0,0 +1,29 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		//What will print to the console when the following code runs?
+
+		int num1 = 5; 
+		int num2 = 2; 
+		
+		System.out.println(num1/num2);
+
+		//What Java data type is most commonly used for whole numbers?
+		//int 
+
+		//What will print to the console when the following code runs?
+		int number1 = 5; 
+		int number2 = 2; 
+		System.out.println(number1 % number2);
+
+		//What Java data type is most commonly used for textual data such as a name, address, or message?
+		//String 
+		
+		//Can you add two Strings together?
+		//Yes
+
+
+	}
+
+}
